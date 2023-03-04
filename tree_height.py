d045838aa6cce946fe45cf109608597b84dbdec2
Deniss Bondars 221RDB122 <124886980+DenissBondars@users.noreply.github.com>
2023-03-04 18:07:09 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -9,18 +9,54 @@ def compute_height(n, parents):
     # Write this function
     max_height = 0
     # Your code here
+    roots = np.argwhere(parents == -1).item()
+    sort_out = np.array([roots])
+    keep_track_head = 0
+    keep_track_back = 1
+
+    tested_elements = np.empty(n, dtype=bool)
+    count_height = np.empty(n, dtype=int)
+    
+    tested_elements[roots] = True
+    count_height[roots] = 1
+
+    while keep_track_head < keep_track_back:
+        current_element = sort_out[keep_track_head]
+        keep_track_head = keep_track_head + 1
+        descendants = np.flatnonzero(parents == current_element)
+        for i in descendants:
+            tested_elements[i] = True
+            count_height[i] = count_height[i] + 1
+            sort_out = np.append(sort_out, i)
+            keep_track_back = keep_track_back + 1
+        
+    max_height = np.max(count_height)
+
     return max_height
 
 
 def main():
     # implement input form keyboard and from files
-    
     # let user input file name to use, don't allow file names with letter a
     # account for github input inprecision
-    
     # input number of elements
     # input values in one variable, separate with space, split these values in an array
     # call the function and output it's result
+    FI = input()
+    if "I" in FI:
+        n = int(input())
+        parents = input()
+        parents = np.array(parents.split(), dtype = int)
+    if "F" in FI:
+        test_file = input()
+        if "a" in test_file:
+            return
+        else:
+            n = int(test_file.readline())
+            parents = test_file.readline()
+            parents = np.array(parents.split(), dtype = int)
+    result = compute_height(n, parents)
+    print(result)
     pass
 
 # In Python, the default limit on recursion depth is rather low,
